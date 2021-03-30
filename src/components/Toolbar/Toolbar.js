781e48391b7db475f49a4b29b2c1d178e187e4e1
Ryosuke Nakano <nakano.ryosuke7@gmail.com>
2021-03-30 22:42:24 +0800

    Adds "Articles" button

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import './Toolbar.css';
-
+import { NavLink } from 'react-router-dom';
 
 const toolbar = props => (
   <header className="navbar">
     <nav>
       <div className="nav-items">
         <ul>
-          <li><a href="#about-part">About</a></li>
-          <li><a href="#projects-part">Projects</a></li>
+          <li><NavLink to='/'>Home</NavLink></li>
+          <li><NavLink to='/articles'>Articles</NavLink></li>
+          <li><a href="/#about-part">About</a></li>
+          <li><a href="/#projects-part">Projects</a></li>
         </ul>
       </div>
     </nav>
