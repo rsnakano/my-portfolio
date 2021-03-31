ab6abb86961666547a60f8dcec5f9d4abc4eb3d7
Ryosuke Nakano <nakano.ryosuke7@gmail.com>
2021-03-31 11:37:03 +0800

    Adds routes for article pieces

--- src/Main.js
+++ src/Main.js
@@ -3,12 +3,16 @@ import { Switch, Route } from 'react-router-dom';
 
 import Home from './pages/Home/Home';
 import Articles from './pages/Articles/Articles';
+import Piece1 from './pages/Articles/Content/Piece1/Piece1';
+import Piece2 from './pages/Articles/Content/Piece2/Piece2';
 
 const Main = () => {
   return (
     <Switch>
       <Route exact path='/' component={Home}></Route>
       <Route exact path='/articles' component={Articles}></Route>
+      <Route exact path='/articles/building-portfolio' component={Piece1}></Route>
+      <Route exact path='/articles/first-hackathon' component={Piece2}></Route>
     </Switch>
   );
 }
